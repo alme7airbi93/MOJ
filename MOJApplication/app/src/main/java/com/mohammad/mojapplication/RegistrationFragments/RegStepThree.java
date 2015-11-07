65394a562180c6277e9120a2755271fcf1f70847
3BBUAE <ali.saif.alzaabi@gmail.com>
2015-11-07 17:28:38 +0400

    copy and pasted

--- MOJApplication/app/src/main/java/com/mohammad/mojapplication/RegistrationFragments/RegStepThree.java
+++ MOJApplication/app/src/main/java/com/mohammad/mojapplication/RegistrationFragments/RegStepThree.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.mohammad.mojapplication.Communicator;
 import com.mohammad.mojapplication.MOJManager;
 import com.mohammad.mojapplication.MainActivity;
 import com.mohammad.mojapplication.Objects.NIDCard;
@@ -27,6 +28,7 @@ public class RegStepThree extends Fragment
     private Button btnFinish;
     private MOJManager mojManager;
     private NIDCard nidCard;
+    private Communicator comm;
 
     public void reciveNIDCardObject(NIDCard nidCard)
     {
@@ -60,11 +62,27 @@ public class RegStepThree extends Fragment
                     User user = new User(nidCard.getId(), nidCard.getName(), nidCard.getMobile(),
                             nidCard.getAddress(), etUserName.getText().toString(), etPass.getText().toString());
                     mojManager.addUser(user);
-                    Intent intent = new Intent(getActivity(), MainActivity.class);
-                    getActivity().startActivity(intent);
-                    getActivity().finish();
-
                     Toast.makeText(getActivity(),"Your Account Added",Toast.LENGTH_LONG).show();
+
+                    try{
+
+                        user = mojManager.findUserByUserName(etUserName.getText().toString());
+                        String pass = user.getPass().toString();
+                        if(user != null)
+                        {
+                            if(pass.equals(etPass.getText().toString()))
+                            {
+                                comm.sendUsertoMainActivity(user);
+                            }else
+                            {
+                                Toast.makeText(getActivity(),etPass.getText().toString() +" password incorrect" + pass,Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                    catch (NullPointerException npe)
+                    {
+                        Toast.makeText(getActivity(),"Username or password incorrect",Toast.LENGTH_LONG).show();
+                    }
 
                 }
             }
