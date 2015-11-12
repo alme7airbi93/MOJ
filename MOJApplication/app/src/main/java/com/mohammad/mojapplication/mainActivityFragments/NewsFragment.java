8a5759971a702e191f695d28190d8a7de2783bb0
alme7airbi93 <alme7airbi93@live.com>
2015-11-12 15:48:51 +0400

    Twitter Available in news fragment

--- MOJApplication/app/src/main/java/com/mohammad/mojapplication/mainActivityFragments/NewsFragment.java
+++ MOJApplication/app/src/main/java/com/mohammad/mojapplication/mainActivityFragments/NewsFragment.java
@@ -4,16 +4,36 @@ package com.mohammad.mojapplication.mainActivityFragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+
+import com.mohammad.mojapplication.MainActivity;
 import com.mohammad.mojapplication.R;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.TweetUtils;
+import com.twitter.sdk.android.tweetui.TweetView;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by user on 10/31/2015.
  */
 public class NewsFragment extends Fragment {
+
+    private LinearLayout tweetLayout;
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "EBzSEIofSqYUFPzRxsf3P9kQH";
+    private static final String TWITTER_SECRET = "1NpVeVnoHEmtTyY7CDnUQkadecm5ueP8QxDbUn1xiYt1dZK8mm";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +43,32 @@ public class NewsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.news_fragment_layout,container,false);
+        View v = inflater.inflate(R.layout.news_fragment_layout,container,false);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(getActivity(), new Twitter(authConfig));
+
+        tweetLayout = (LinearLayout) v.findViewById(R.id.tweet_layout);
+
+
+        // TODO: Use a more specific parent
+        final ViewGroup parentView = (ViewGroup)getActivity().getWindow().getDecorView().getRootView();
+        // TODO: Base this Tweet ID on some data from elsewhere in your app
+        long tweetId = 631879971628183552L;
+        TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
+            @Override
+            public void success(Result<Tweet> result) {
+                TweetView tweetView = new TweetView(getActivity(), result.data);
+                tweetLayout.addView(tweetView);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Load Tweet failure", exception);
+            }
+        });
+
+
+        return v;
+
     }
 }
