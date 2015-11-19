0874e543dfbbbc94621fcef1a13ea0910a525a14
alme7airbi93 <alme7airbi93@live.com>
2015-11-19 19:48:30 +0400

    TWITTER WORKS

--- MOJApplication/app/src/main/java/com/mohammad/mojapplication/mainActivityFragments/NewsFragment.java
+++ MOJApplication/app/src/main/java/com/mohammad/mojapplication/mainActivityFragments/NewsFragment.java
@@ -1,9 +1,11 @@
 package com.mohammad.mojapplication.mainActivityFragments;
 
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,19 +19,30 @@ import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.TweetUi;
 import com.twitter.sdk.android.tweetui.TweetUtils;
 import com.twitter.sdk.android.tweetui.TweetView;
+import com.twitter.sdk.android.tweetui.UserTimeline;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by user on 10/31/2015.
  */
-public class NewsFragment extends Fragment {
+
+
+public class NewsFragment extends ListFragment {
 
     private LinearLayout tweetLayout;
+    private  ArrayList<Tweet> tweets;
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "EBzSEIofSqYUFPzRxsf3P9kQH";
@@ -38,35 +51,33 @@ public class NewsFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.news_fragment_layout,container,false);
+        View v = inflater.inflate(R.layout.news_fragment_layout, container, false);
+
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(getActivity(), new Twitter(authConfig));
-
-        tweetLayout = (LinearLayout) v.findViewById(R.id.tweet_layout);
+        Fabric.with(getActivity(), new TwitterCore(authConfig), new TweetUi());
 
 
-        // TODO: Use a more specific parent
-        final ViewGroup parentView = (ViewGroup)getActivity().getWindow().getDecorView().getRootView();
-        // TODO: Base this Tweet ID on some data from elsewhere in your app
-        long tweetId = 631879971628183552L;
-        TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
-            @Override
-            public void success(Result<Tweet> result) {
-                TweetView tweetView = new TweetView(getActivity(), result.data);
-                tweetLayout.addView(tweetView);
-            }
 
-            @Override
-            public void failure(TwitterException exception) {
-                Log.d("TwitterKit", "Load Tweet failure", exception);
-            }
-        });
-
+        final UserTimeline userTimeline = new UserTimeline.Builder()
+                .screenName("moj_uae")
+                .build();
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getActivity())
+                .setTimeline(userTimeline)
+                .build();
+        setListAdapter(adapter);
 
         return v;
 
